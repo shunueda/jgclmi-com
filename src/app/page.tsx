a9500be719c839bee22fd162adcb76072519fd2c
Shun Ueda <me@shu.nu>
2024-10-22 14:12:07 -0400

    feat(home): add pickup articles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,11 +13,12 @@ import { asTextLines, getAllByType, getSingleByType } from '#lib/prismic'
 import { cn } from '#lib/utils'
 
 export default async function Home() {
-  const { data } = await getSingleByType(SinglePrismicType.HOME)
   const today = new Date()
+  const { data } = await getSingleByType(SinglePrismicType.HOME)
   const news = (await getAllByType(RepeatablePrismicType.NEWS)).filter(
     ({ data }) => new Date(data.display_until_date) >= today
   )
+  const articles = await getAllByType(RepeatablePrismicType.ARTICLE)
   const email = asLink(data.email)
   const links = [
     {
@@ -77,13 +78,34 @@ export default async function Home() {
           </Table>
         </section>
       )}
+      {articles.length > 0 && (
+        <section className='mt-8'>
+          <h2>注目記事</h2>
+          <Table className='mt-4'>
+            <TableBody>
+              {articles.map(it => (
+                <TableRow key={it.id}>
+                  <TableCell className=' text-xs'>
+                    {formatDate(new Date(it.last_publication_date))}
+                  </TableCell>
+                  <TableCell>
+                    <Link href={`/article/${it.id}`}>
+                      {asText(it.data.title)}
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </section>
+      )}
       <section className='mt-8'>
         <h2>お問合せ</h2>
         <p className='mt-4'>
           下記のメールアドレスより、お気軽にご連絡ください。
         </p>
-        <p className='mt-2'>
-          <MailIcon className='inline mr-2 mb-0.5' size={16} />
+        <p className='mt-2 text-sm'>
+          <MailIcon className='inline mr-2' size={12} />
           <a href={`mailto:${email}`} target='_blank' rel='noreferrer'>
             {email}
           </a>
