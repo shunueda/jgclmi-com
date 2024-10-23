485e47ceac8c15f909d08feae4454b7149a96460
Shun Ueda <me@shu.nu>
2024-10-22 23:29:14 -0400

    feat(manna): add manna list

--- src/app/manna/page.tsx
+++ src/app/manna/page.tsx
@@ -1,13 +1,18 @@
 import {
   Table,
   TableBody,
+  TableCell,
   TableHead,
   TableHeader,
   TableRow
 } from '@/components/ui/table'
+import data from '~/public/manna/data.json'
 import { Template } from '#components/template'
+import { range } from '#lib/utils'
 
 export default async function Page() {
+  const info = new Map<number, (typeof data)[number]>()
+  data.forEach(it => info.set(it.number, it))
   return (
     <Template
       title='Manna'
@@ -22,7 +27,35 @@ export default async function Page() {
             <TableHead className='text-center'>トピック</TableHead>
           </TableRow>
         </TableHeader>
-        <TableBody />
+        <TableBody>
+          {range(1, 145)
+            .reverse()
+            .map(number => {
+              const { date, title, topics } = info.get(number) || {
+                number,
+                date: '不明',
+                title: '不明',
+                topic: '不明'
+              }
+              const padded = number.toString().padStart(3, '0')
+              return (
+                <TableRow key={number}>
+                  <TableCell className='text-center'>{number}</TableCell>
+                  <TableCell className='text-center'>{date}</TableCell>
+                  <TableCell className='text-center'>
+                    <a
+                      target='_blank'
+                      href={`/manna/manna${padded}.pdf`}
+                      rel='noreferrer'
+                    >
+                      {title}
+                    </a>
+                  </TableCell>
+                  <TableCell className='text-center'>{topics}</TableCell>
+                </TableRow>
+              )
+            })}
+        </TableBody>
       </Table>
     </Template>
   )
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function range(start: number, end: number) {
+  return Array.from({ length: end - start + 1 }, (_, i) => start + i)
+}
